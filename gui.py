dda5bbc826fce7a135f8290955fac7584caac576
Zebulon <zeb@zebulon.org.uk>
2022-02-24 11:27:42 +0100

    rewrite for new OOP based tekh

--- gui.py
+++ gui.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import tekh
-import numpy as np
 
 win = Tk()  # creating the main window and storing the window object in 'win'
 win.title('Tekhenu Bot Ra action')  # setting title of the window
@@ -9,42 +8,48 @@ win.title('Tekhenu Bot Ra action')  # setting title of the window
 # win.geometry('500x200')  # setting the size of the window
 
 
-def update_scores(bplist, bhvlist, bhhlist, tpl, bldv, bldh):
+def update_scores(bplist, bhvlist, bhhlist, tpl):
     for i in range(5):
         if bhvlist[i]['text'] == 'B':
-            bldv[i] = 9
+            tpl.set_house_v(i, tekh.Owner.bot)
         elif bhvlist[i]['text'] == 'P':
-            bldv[i] = 1
+            tpl.set_house_v(i, tekh.Owner.player)
         elif bhvlist[i]['text'] == '0':
-            bldv[i] = 0
+            tpl.set_house_v(i, tekh.Owner.none)
         if bhhlist[i]['text'] == 'B':
-            bldh[i] = 9
+            tpl.set_house_h(i, tekh.Owner.bot)
         elif bhhlist[i]['text'] == 'P':
-            bldh[i] = 1
+            tpl.set_house_h(i, tekh.Owner.player)
         elif bhhlist[i]['text'] == '0':
-            bldh[i] = 0
+            tpl.set_house_h(i, tekh.Owner.none)
 
-    tekh.set_base_scores(tpl)
     for i in range(5):
         for j in range(5):
-            bplist[5 * i + j].config(bg='#F0F0F0')
-            if bplist[5 * i + j]['text'] == 'I':
-                tpl[i, j] = 9
-                bplist[5 * i + j].config(bg='light green')
-    tekh.update_scores(tpl, bldv, bldh)
-    #tekh.display_all(tpl, bldv, bldh)
+            if bplist[5 * i + j]['text'] == 'B':
+                tpl.set_tile(i, j, tekh.Owner.bot)
+                bplist[5 * i + j].config(bg='pink')
+                bplist[5 * i + j].config(text='I')  # replace pillar name with I regardless of ownership
+            elif bplist[5 * i + j]['text'] == 'P':
+                tpl.set_tile(i, j, tekh.Owner.player)
+                bplist[5 * i + j].config(bg='light blue')
+                bplist[5 * i + j].config(text='I')  # replace pillar name with I regardless of ownership
+            elif bplist[5 * i + j]['text'] == '0':
+                tpl.set_tile(i, j, tekh.Owner.none)
+                bplist[5 * i + j].config(bg='#F0F0F0')
+
     for i in range(5):
         for j in range(5):
-            if tpl[i,j] == 9:
-                bplist[5 * i + j].config(text='I')
-            else:
-                bplist[5 * i + j].config(text=str(tpl[i,j]))
+            if tpl.get_tile(i, j).owner == tekh.Owner.none:  # put new scores in non built tiles
+                bplist[5 * i + j].config(text=str(tpl.get_score(i, j)))
+                bplist[5 * i + j].config(bg='#F0F0F0')  # reset colour to grey (removes previous red)
 
     return
 
 
-def func_house(button, bplist, bhvlist, bhhlist, tpl, bldv, bldh):  # function of the button
+def func_house(button, bplist, bhvlist, bhhlist, tpl):  # function of the button
     dialog = Toplevel()
+    dialog.title("Place a House")
+    dialog.geometry('250x100')
     choice = StringVar()
     r1 = Radiobutton(dialog, text="Bot", variable=choice, value='B')
     r2 = Radiobutton(dialog, text="Player", variable=choice, value='P')
@@ -53,84 +58,82 @@ def func_house(button, bplist, bhvlist, bhhlist, tpl, bldv, bldh):  # function o
     r1.pack(anchor=W)
     r2.pack(anchor=W)
     r3.pack(anchor=W)
+    okButton = Button(dialog, text='OK', command=dialog.destroy)
+    okButton.pack()
     dialog.wait_visibility()
     dialog.grab_set()
     dialog.wait_window()
     button.config(text=choice.get())
     if choice.get() == 'B':
-        button.config(bg = 'pink')
+        button.config(bg='pink')
     elif choice.get() == 'P':
         button.config(bg='light blue')
     else:
         button.config(bg='#F0F0F0')
-    update_scores(bplist, bhvlist, bhhlist, tpl, bldv, bldh)
+    update_scores(bplist, bhvlist, bhhlist, tpl)
+
     return
 
 
-def func_pillar(button, btnpil_list, btnhv_list, btnhh_list, tpl, bldv, bldh):
+def func_pillar(button, btnpil_list, btnhv_list, btnhh_list, tpl):
     dialog = Toplevel()
+    dialog.title("Place a Pillar")
+    dialog.geometry('250x100')
     choice = StringVar()
-    r1 = Radiobutton(dialog, text="Bot", variable=choice, value='I')
-    r2 = Radiobutton(dialog, text="Player", variable=choice, value='I')
+    r1 = Radiobutton(dialog, text="Bot", variable=choice, value='B')
+    r2 = Radiobutton(dialog, text="Player", variable=choice, value='P')
     r3 = Radiobutton(dialog, text="None", variable=choice, value='0')
     choice.set('0')
     r1.pack(anchor=W)
     r2.pack(anchor=W)
     r3.pack(anchor=W)
+    okButton = Button(dialog, text='OK', command=dialog.destroy)
+    okButton.pack()
     dialog.wait_visibility()
     dialog.grab_set()
     dialog.wait_window()
     button.config(text=choice.get())
     if choice.get() == 'B':
-        button.config(bg = 'pink')
+        button.config(bg='pink')
     elif choice.get() == 'P':
         button.config(bg='light blue')
     else:
         button.config(bg='#F0F0F0')
-    update_scores(btnpil_list, btnhv_list, btnhh_list, tpl, bldv, bldh)
+    update_scores(btnpil_list, btnhv_list, btnhh_list, tpl)
     return
 
 
-def choose(bplist, bhvlist, bhhlist, tpl, bldv, bldh, lbl):
+def choose(bplist, bhvlist, bhhlist, tpl, msgbox):
     for i in range(5):
         if bhvlist[i]['text'] == 'B':
-            bldv[i] = 9
+            tpl.set_house_v(i, tekh.Owner.bot)
         elif bhvlist[i]['text'] == 'P':
-            bldv[i] = 1
+            tpl.set_house_v(i, tekh.Owner.player)
         elif bhvlist[i]['text'] == '0':
-            bldv[i] = 0
+            tpl.set_house_v(i, tekh.Owner.none)
         if bhhlist[i]['text'] == 'B':
-            bldh[i] = 9
+            tpl.set_house_h(i, tekh.Owner.bot)
         elif bhhlist[i]['text'] == 'P':
-            bldh[i] = 1
+            tpl.set_house_h(i, tekh.Owner.player)
         elif bhhlist[i]['text'] == '0':
-            bldh[i] = 0
-    tekh.set_base_scores(tpl)
+            tpl.set_house_h(i, tekh.Owner.none)
     for i in range(5):
         for j in range(5):
-            bplist[5 * i + j].config(bg='#F0F0F0')
-            if bplist[5 * i + j]['text'] == 'I':
-                tpl[i, j] = 9
-                bplist[5 * i + j].config(bg='light green')
-    tekh.update_scores(tpl, bldv, bldh)
-    #tekh.display_all(tpl, bldv, bldh)
-    (msg, choice) = tekh.choose_place(tpl, bldv, bldh)
-    lbl.delete(1.0, END)
-    lbl.insert(END, msg)
+            if bplist[5 * i + j]['text'] != 'I':
+                bplist[5 * i + j].config(bg='#F0F0F0')
+    (msg, choice) = tpl.choose_place()
+    msgbox.delete(1.0, END)
+    msgbox.insert(END, msg)
     bplist[5 * choice[0] + choice[1]].config(bg='red')
     return
 
 
 # build temple
-# tekh
-tpl = np.zeros((5, 5), dtype=np.int8)
-bldv = np.zeros(5, dtype=np.int8)
-bldh = np.zeros(5, dtype=np.int8)
-tekh.set_base_scores(tpl)
+temple = tekh.Temple()
 
-btnhv_list = []
-btnhh_list = []
-btnpil_list = []
+btn_house_v_list = []
+btn_house_h_list = []
+btn_pillar_list = []
 
 top_frame = Frame(win)
 temple_frame = Frame(top_frame, highlightbackground="blue", highlightthickness=2)
@@ -138,34 +141,36 @@ top_frame.grid(row=0)
 temple_frame.grid(column=1, rowspan=5, columnspan=5)
 # create vertical house buttons
 for i in range(5):
-    btn = Button(top_frame, text=str(bldv[i]), width=5, height=2)
-    btn["command"] = lambda btn=btn: func_house(btn, btnpil_list, btnhv_list, btnhh_list, tpl, bldv, bldh)
-    btnhv_list.append(btn)
+    btn = Button(top_frame, text=str(temple.get_house_v(i)), width=5, height=2)
+    btn["command"] = lambda btn=btn: func_house(btn, btn_pillar_list, btn_house_v_list, btn_house_h_list, temple)
+    btn_house_v_list.append(btn)
     btn.grid(row=i, column=0)
 
 # create horizontal house buttons
 for i in range(5):
-    btn = Button(top_frame, text=str(bldh[i]), width=5, height=2)
-    btn["command"] = lambda btn=btn: func_house(btn, btnpil_list, btnhv_list, btnhh_list, tpl, bldv, bldh)
-    btnhh_list.append(btn)
+    btn = Button(top_frame, text=str(temple.get_house_h(i)), width=5, height=2)
+    btn["command"] = lambda btn=btn: func_house(btn, btn_pillar_list, btn_house_v_list, btn_house_h_list, temple)
+    btn_house_h_list.append(btn)
     btn.grid(row=5, column=i+1)
 
 # create pillar tile buttons
 for i in range(5):
     for j in range(1, 6):
-        btn = Button(temple_frame, text=str(tpl[i,j-1]), width=5, height=2)
-        btn["command"] = lambda btn=btn: func_pillar(btn, btnpil_list, btnhv_list, btnhh_list, tpl, bldv, bldh)
-        btnpil_list.append(btn)
+        btn = Button(temple_frame, text=str(temple.get_tile(i, j - 1)), width=5, height=2)
+        btn["command"] = lambda btn=btn: func_pillar(btn, btn_pillar_list, btn_house_v_list, btn_house_h_list, temple)
+        btn_pillar_list.append(btn)
         btn.grid(row=i, column=j)
 
+update_scores(btn_pillar_list, btn_house_v_list, btn_house_h_list, temple)
 bottom_frame = Frame(win)
 bottom_frame.grid(row=2)
 
-lbl = Text(bottom_frame, height=18, width=60)
-lbl.configure(font=("Helvetica", 8))
+msgbox = Text(bottom_frame, height=18, width=60)
+msgbox.configure(font=("Helvetica", 8))
 btn_choose = Button(bottom_frame, text='Find bot pillar place', height=2)
-btn_choose["command"] = lambda btn_choose=btn_choose: choose(btnpil_list, btnhv_list, btnhh_list, tpl, bldv, bldh, lbl)
+btn_choose["command"] = lambda btn_choose=btn_choose: choose(btn_pillar_list, btn_house_v_list, btn_house_h_list,
+                                                             temple, msgbox)
 btn_choose.grid(row=6, columnspan=6)
-lbl.grid(columnspan=6)
+msgbox.grid(columnspan=6)
 
 win.mainloop()  # running the loop that works as a trigger
